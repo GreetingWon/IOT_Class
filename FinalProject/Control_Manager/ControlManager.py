293be8bd4b3f42d62759f1ad5d2e99267a036ec0
WonSeongMin <90883558+GreetingWon@users.noreply.github.com>
2022-09-16 00:21:19 +0900

    Create ControlManager.py

---
+++ FinalProject/Control_Manager/ControlManager.py
@@ -0,0 +1,27 @@
+import os
+import paho.mqtt.client as mqtt
+import sys
+from datetime import datetime
+import requests
+import json
+
+topic = "pi/#"
+server = "34.193.131.206" #EC2 address
+
+def on_connect(client, userdata, flags, rc):
+    print("Start Program...")
+    client.subscribe(topic)
+
+def on_message(client, userdata, msg):
+    print(msg.payload)
+    if(int(msg.payload) == 1):
+        os.system("python3 /home/ubuntu/Control_Magager/Texting.py") #Texting.py dir 
+    elif(int(msg.payload) == 2) :
+        os.system("python3 /home/ubuntu/Control_Magager/weather.py") #weather.py dir
+
+client =mqtt.Client()
+client.connect(server, 1883, 60)
+client.on_connect = on_connect
+client.on_message = on_message
+client.subscribe("pi/#")
+client.loop_forever()
